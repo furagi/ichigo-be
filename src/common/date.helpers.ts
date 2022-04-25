75c7bf1af69aadcae564c856140eaaf45ff2f50f
Ilya Boyko <furagi.usagi@gmail.com>
2022-04-25 23:59:03 +0400

    emplemented getDateWeek helper

--- src/common/date.helpers.ts
+++ src/common/date.helpers.ts
@@ -1,3 +1,16 @@
 export function getDateWeek(date: Date): Date[] {
-  return [date];
+  // so for 04.25th 2022 we get 25 - 1
+  const sunday = date.getDate() - date.getDay();
+  const week = [];
+  for (let i = 0; i < 7; i++) {
+    const day = new Date(date);
+    day.setDate(sunday + i);
+    /*
+     * I cloud get and substract minutes, hours, etc, but instead I'm just subsctracting
+     * the mss and writing this comment
+     */
+    day.setTime(+day - (+day % (1000 * 3600 * 24)));
+    week.push(day);
+  }
+  return week;
 }
