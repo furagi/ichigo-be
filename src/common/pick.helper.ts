403110c8eeef419df47e2399868a240ab799ea83
Ilya Boyko <furagi.usagi@gmail.com>
2022-04-25 23:29:49 +0400

    added tests for pick helper

---
+++ src/common/pick.helper.ts
@@ -0,0 +1,6 @@
+export function pick(
+  object: { [K: string]: unknown },
+  fields: (string | number)[],
+) {
+  return object;
+}
---
+++ src/common/pick.spec.ts
@@ -0,0 +1,12 @@
+import { pick } from './pick.helper';
+
+describe('pick', () => {
+  it('should be defined', () => {
+    expect(pick).toBeDefined();
+  });
+
+  it('should select only passed values', () => {
+    const testObject = { a: 1, b: 2, 3: 4 };
+    expect(pick(testObject, ['a', 3])).toEqual({ a: 1, 3: 4 });
+  });
+});
